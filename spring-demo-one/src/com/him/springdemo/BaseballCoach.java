273a8bf01ec8beca71ca65be212404df78c1da77
himanshuKp <cse10324.sbit@gmail.com>
2019-12-10 01:26:50 +0530

    constructor injection

--- spring-demo-one/src/com/him/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/him/springdemo/BaseballCoach.java
@@ -2,8 +2,23 @@ package com.him.springdemo;
 
 public class BaseballCoach implements Coach{
 
+  //define a private field for the dependency
+  private FortuneService fortuneService;
+  
+  //define a constructor for dependency injection
+  public BaseballCoach(FortuneService theFortuneService) {
+    this.fortuneService = theFortuneService;
+  }
+  
   @Override
   public String getDailyWorkout() {
     return "spend 30 minutes on batting practice";
+  }
+
+  @Override
+  public String getDailyFortune() {
+    //use my fortuneService to get a fortune
+    //dependency = helper
+    return fortuneService.getFortune();
   }
 }
--- spring-demo-one/src/com/him/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/him/springdemo/TrackCoach.java
@@ -2,9 +2,28 @@ package com.him.springdemo;
 
 public class TrackCoach implements Coach {
 
+  //define a private field for dependency
+  private FortuneService fortuneService;
+
+  public TrackCoach() {
+    
+  }
+  
+  //define a constructor for dependency injection
+  public TrackCoach(FortuneService theFortuneService) {
+    this.fortuneService = theFortuneService;
+  }
+  
   @Override
   public String getDailyWorkout() {
      return "Give me a hard 5K";
+  }
+
+  @Override
+  public String getDailyFortune() {
+    //use my fortuneService to get a fortune
+    //dependency = helper
+    return "Just Do It: "+ fortuneService.getFortune();
   }
 
 }
