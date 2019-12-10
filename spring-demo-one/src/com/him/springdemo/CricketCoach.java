c8ab92fbd6495bad544f383be39d705738c73283
himanshuKp <cse10324.sbit@gmail.com>
2019-12-11 00:25:43 +0530

    injecting literal values

--- spring-demo-one/src/com/him/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/him/springdemo/CricketCoach.java
@@ -3,6 +3,8 @@ package com.him.springdemo;
 public class CricketCoach implements Coach{
   
   private FortuneService fortuneService;
+  private String emailAddress;
+  private String team;
   
   public CricketCoach() {
     System.out.println("Inside no-arg cricketcoach constructor");
@@ -11,6 +13,22 @@ public class CricketCoach implements Coach{
   public void setFortuneService(FortuneService fortuneService) {
   System.out.println("Inside setter method - setFortuneService");
     this.fortuneService = fortuneService;
+  }
+  
+  public void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
+
+  public void setTeam(String team) {
+    this.team = team;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public String getTeam() {
+    return team;
   }
 
   @Override
