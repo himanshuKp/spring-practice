b58d1e1eb2ebe640653eb9adcd85f1ad575205bd
himanshuKp <cse10324.sbit@gmail.com>
2019-12-11 00:54:35 +0530

    generate random fortunes

--- spring-demo-one/src/com/him/springdemo/HappyFortuneService.java
+++ spring-demo-one/src/com/him/springdemo/HappyFortuneService.java
@@ -1,10 +1,19 @@
 package com.him.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
 
+  private String myFortunes[] = {"My first fortune",
+                               "My second fortune",
+                               "My Third Fortune"};
+  
   @Override
   public String getFortune() {
-    return "Today is your lucky day";
+    
+    int randomInt = new Random().nextInt(3);
+    return myFortunes[randomInt];
+    
   }
 
 }
