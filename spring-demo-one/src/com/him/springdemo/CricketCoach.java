65b4f395827804ade173d3ef89384f0bb6a32f34
himanshuKp <cse10324.sbit@gmail.com>
2019-12-10 23:42:44 +0530

    setter injection

--- spring-demo-one/src/com/him/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/him/springdemo/CricketCoach.java
@@ -8,7 +8,8 @@ public class CricketCoach implements Coach{
     System.out.println("Inside no-arg cricketcoach constructor");
   }
  
-  protected void setFortuneService(FortuneService fortuneService) {
+  public void setFortuneService(FortuneService fortuneService) {
+  System.out.println("Inside setter method - setFortuneService");
     this.fortuneService = fortuneService;
   }
 
