297e110ca31d90b0d0828b9c864a945c88bad84a
himanshuKp <cse10324.sbit@gmail.com>
2019-12-13 06:52:42 +0530

    usage of @Qualifier annotation and field injection

--- spring-demo-annotations/src/com/him/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/him/springdemo/TennisCoach.java
@@ -1,27 +1,32 @@
 package com.him.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+  @Autowired
+  @Qualifier("happyFortuneService")
   private FortuneService fortuneService;
+  
+  public TennisCoach() {
+    System.out.println("TennisCoach: Inside default constructor");
+  }
   
   /*
    * @Autowired public TennisCoach(FortuneService theFortuneService) {
    * fortuneService = theFortuneService; }
    */
   
-  public TennisCoach() {
-    System.out.println("TennisCoach: Inside default constructor");
-  }
-  
+  /*
   @Autowired
-  public void setFortuneService(FortuneService fortuneService) {
-    System.out.println("Inside setter method for tennis coach : setFortuneService");
+  public void doFortuneService(FortuneService fortuneService) {
+    System.out.println("Inside setter method for tennis coach : doFortuneService");
     this.fortuneService = fortuneService;
   }
+  */
   
   @Override
   public String getDaillyWorkout() {
